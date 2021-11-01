ca5b9ae81cd25bdfaa0ba5612885cc347713b0cf
“Rachiey” <rachaelyonoff@gmail.com>
2021-11-01 12:30:12 +0000

    added ds store to gitignore

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Greeting from './components/Greeting';
+import Jokes from './components/Jokes';
+
+function App(){
+    return (
+        <>
+        <Greeting />
+        <Jokes />
+        </>
+    )
+}
+
+
+export default App;
